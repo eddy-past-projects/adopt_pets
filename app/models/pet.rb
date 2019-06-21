3f0c8a94f96dfc44acd5efc492fc83c2e6150fac
github username <github email address>
2019-06-21 17:44:20 -0400

    new user js works

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,2 +1,7 @@
 class Pet < ApplicationRecord
+  belongs_to :user
+
+
+
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,35 @@
 class User < ApplicationRecord
-end
+  has_secure_password
+
+  has_many :pets
+
+
+  validates :name, uniqueness: true
+
+
+
+
+  # def self.away?
+  #   self.all.each do |person|
+  #     if person.home == true
+  #       person.home = false
+  #     else
+  #       person.home = true
+  #     end
+  #   end
+  # end
+  #
+  # def self.most_pets
+  #   @sort_pets = self.all.sort_by do |person|
+  #     person.pets.length
+  #
+  #   end
+  #   @sort_pets
+  # end
+
+
+
+
+
+
+end #end of class
